51ecea5533f687038b54d09fadbb546b45dfa18d
FelipePontin <inf00353@sky.com.br>
2025-10-04 19:53:37 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {};
+
+const Critters = require("critters-webpack-plugin");
+
+module.exports = {
+  webpack: (config: { plugins: any[] }) => {
+    config.plugins.push(
+      new Critters({
+        preload: "swap",
+        compress: true,
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
